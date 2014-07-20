a9593ce9385cbc39b84b366cd7523f6a5992ecce
Jerome Cheng <jerome@ayulin.net>
2014-07-20 18:32:18 +0800

    Add ForecastApi.

---
+++ Forecast.io PCL/ForecastApi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable
+{
+    public class ForecastApi
+    {
+        private string apiKey;
+
+        public ForecastApi(string key)
+        {
+            this.apiKey = key;
+        }
+    }
+}
