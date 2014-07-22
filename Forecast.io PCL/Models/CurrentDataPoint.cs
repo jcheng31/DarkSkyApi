2c4738fa236dccb50164ae4e4d7918013230fdbe
Jerome Cheng <jerome@ayulin.net>
2014-07-22 12:01:56 +0800

    Add CurrentDataPoint class.

---
+++ Forecast.io PCL/Models/CurrentDataPoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    [DataContract]
+    public class CurrentDataPoint
+    {
+        [DataMember]
+        private int time;
+
+        [DataMember(Name = "Summary")]
+        public string summary;
+
+        [DataMember(Name = "Icon")]
+        public string icon;
+
+        [DataMember(Name = "NearestStormDistance")]
+        public float nearestStormDistance;
+
+        [DataMember(Name = "NearestStormBearing")]
+        public float nearestStormBearing;
+
+        [DataMember(Name = "PrecipitationIntensity")]
+        public float precipIntensity;
+
+        [DataMember(Name = "PrecipitationProbability")]
+        public float precipProbability;
+
+        [DataMember(Name = "Temperature")]
+        public float temperature;
+
+        [DataMember(Name = "ApparentTemperature")]
+        public float apparentTemperature;
+
+        [DataMember(Name = "DewPoint")]
+        public float dewPoint;
+
+        [DataMember(Name = "Humidity")]
+        public float humidity;
+
+        [DataMember(Name = "WindSpeed")]
+        public float windSpeed;
+
+        [DataMember(Name = "WindBearing")]
+        public float windBearing;
+
+        [DataMember(Name = "Visibility")]
+        public float visibility;
+
+        [DataMember(Name = "CloudCover")]
+        public float cloudCover;
+
+        [DataMember(Name = "Pressure")]
+        public float pressure;
+
+        [DataMember(Name = "Ozone")]
+        public float ozone;
+    }
+}
