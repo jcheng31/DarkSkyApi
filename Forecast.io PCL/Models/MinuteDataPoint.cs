e95b6caa90cb9528d053412fc79f04a70af511e2
Jerome Cheng <jerome@ayulin.net>
2014-07-22 15:13:27 +0800

    Add MinuteDataPoint class.

---
+++ Forecast.io PCL/Models/MinuteDataPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    [DataContract]
+    public class MinuteDataPoint
+    {
+        [DataMember]
+        private int time;
+
+        [DataMember(Name = "PrecipitationIntensity")]
+        public float precipIntensity;
+
+        [DataMember(Name = "PrecipitationProbability")]
+        public float precipProbability;
+    }
+}
