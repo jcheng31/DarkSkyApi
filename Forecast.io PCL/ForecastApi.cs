7dc592e38e18e1a5347214fc572660f2ee48df86
Jerome Cheng <jerome@ayulin.net>
2014-07-22 11:11:42 +0800

    Add API URLs for current and timed forecasts.

--- Forecast.io PCL/ForecastApi.cs
+++ Forecast.io PCL/ForecastApi.cs
@@ -9,6 +9,9 @@ namespace ForecastIOPortable
     {
         private string apiKey;
 
+        private const string currentConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&extend={4}&exclude={5}&lang={6}";
+        private const string specificTimeConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2},{3?units={4}&extend={5}&exclude={6}&lang={7}";
+
         public ForecastApi(string key)
         {
             this.apiKey = key;
