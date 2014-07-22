2e51e8b1d27a485b171955697740bd13cdf3b528
Jerome Cheng <jerome@ayulin.net>
2014-07-22 11:33:29 +0800

    Add enums for the various request parameters.

---
+++ Forecast.io PCL/RequestParameters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable
+{
+    public enum Unit
+    {
+        US, // us
+        SI, // si
+        CA, // ca
+        UK, // uk
+        Auto // auto
+    }
+
+    public enum Exclude
+    {
+        Currently, // currently
+        Minutely, // minutely
+        Hourly, // hourly
+        Daily, // daily
+        Alerts, // alerts
+        Flags // flags
+    }
+
+    public enum Language
+    {
+        German, // de
+        English, // en
+        Spanish, // es
+        French, // fr
+        Dutch, // nl
+        Tetum // tet
+    }
+}
