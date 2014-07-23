3f3aaab813828dc1aeccbe12e8f7275536dd9c06
Jerome Cheng <jerome@ayulin.net>
2014-07-23 11:37:51 +0800

    Add header comment.

--- Forecast.io PCL/Helpers.cs
+++ Forecast.io PCL/Helpers.cs
@@ -7,6 +7,15 @@ namespace ForecastIOPortable
 {
     public class Helpers
     {
+        /// <summary>
+        /// Converts Unix Time (seconds since January 1, 1970) to a DateTime object.
+        /// </summary>
+        /// <param name="secondsSince">
+        /// Seconds since January 1, 1970.
+        /// </param>
+        /// <returns>
+        /// <see cref="DateTime"/> representing the given Unix time.
+        /// </returns>
         public static DateTime UnixTimeToDateTime(int secondsSince)
         {
             var baseTime = new DateTime(1970, 1, 1, 0, 0, 0);
