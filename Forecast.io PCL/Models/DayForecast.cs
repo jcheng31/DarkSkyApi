9b6f38f7cc94b30826c31b79423962678de22103
Jerome Cheng <jerome@ayulin.net>
2014-07-23 15:29:59 +0800

    Add Day- and HourForecast classes.

---
+++ Forecast.io PCL/Models/DayForecast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class DayForecast
+    {
+        [DataMember(Name = "summary")]
+        public string Summary { get; set; }
+
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+
+        [DataMember(Name = "data")]
+        public IList<DayDataPoint> Days { get; set; }
+    }
+}
---
+++ Forecast.io PCL/Models/HourForecast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class HourForecast
+    {
+        [DataMember(Name = "summary")]
+        public string Summary { get; set; }
+
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+
+        
+        [DataMember(Name = "data")]
+        public IList<HourDataPoint> Hours { get; set; } 
+
+    }
+}
