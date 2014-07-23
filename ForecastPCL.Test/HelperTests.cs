6958b20fbfc7fed1d049daf81d5e4be6008bec23
Jerome Cheng <jerome@ayulin.net>
2014-07-23 11:32:37 +0800

    Recreate test project as class library.

---
+++ ForecastPCL.Test/HelperTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForecastPCL.Test
+{
+    using ForecastIOPortable;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class HelperTests
+    {
+        [Test]
+        public void ConvertZero()
+        {
+            var expected = new DateTime(1970, 1, 1, 0, 0, 0);
+            var actual = Helpers.UnixTimeToDateTime(0);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
