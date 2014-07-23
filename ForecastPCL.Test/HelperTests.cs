ff52486bc278ab3b14f4733de51792c7fa66cb1c
Jerome Cheng <jerome@ayulin.net>
2014-07-23 11:43:04 +0800

    Add test for a non-zero date.

--- ForecastPCL.Test/HelperTests.cs
+++ ForecastPCL.Test/HelperTests.cs
@@ -21,5 +21,14 @@ namespace ForecastPCL.Test
 
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [Test]
+        public void ConvertSpecificDate()
+        {
+            var expected = new DateTime(2014, 7, 23, 3, 40, 49);
+            var actual = Helpers.UnixTimeToDateTime(1406086849);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
