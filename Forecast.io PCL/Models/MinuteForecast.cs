ee3da4b3417ac25228f4e6ff1d7891a8c72a8775
Jerome Cheng <jerome@ayulin.net>
2014-07-23 10:17:01 +0800

    Add MinuteForecast class.

---
+++ Forecast.io PCL/Models/MinuteForecast.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    [DataContract]
+    public class MinuteForecast
+    {
+        [DataMember(Name = "Summary")]
+        public string summary { get; set; }
+
+        [DataMember(Name = "Icon")]
+        public string icon { get; set; }
+
+        [DataMember(Name = "Minutes")]
+        public IList<MinuteDataPoint> data { get; set; }
+    }
+}
