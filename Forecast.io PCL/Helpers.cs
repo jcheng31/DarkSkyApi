9c40a2f88e6c2cfa30d58159bae973eab377f799
Jerome Cheng <jerome@ayulin.net>
2014-07-23 11:33:23 +0800

    Add helper class.

---
+++ Forecast.io PCL/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable
+{
+    public class Helpers
+    {
+        public static DateTime UnixTimeToDateTime(int secondsSince)
+        {
+            return new DateTime();
+        }
+    }
+}
