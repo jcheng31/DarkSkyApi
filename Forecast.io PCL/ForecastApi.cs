5f6b5d9929d99805cdee0bd759a6c5675e51cfc5
Jerome Cheng <jerome@ayulin.net>
2014-07-24 11:54:58 +0800

    Add header comments to API URLs.

--- Forecast.io PCL/ForecastApi.cs
+++ Forecast.io PCL/ForecastApi.cs
@@ -7,10 +7,32 @@ namespace ForecastIOPortable
 {
     public class ForecastApi
     {
-        private string apiKey;
+        /// <summary>
+        /// The API endpoint to retrieve current weather conditions.
+        /// {0} - API key.
+        /// {1} - Latitude.
+        /// {2} - Longitude.
+        /// {3} - Units.
+        /// {4} - Extends hourly data to include the next 7 days (if "hourly" is given).
+        /// {5} - Any blocks to be excluded from the results.
+        /// {6} - The language to be used in text summaries.
+        /// </summary>
+        private const string CurrentConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&extend={4}&exclude={5}&lang={6}";
 
-        private const string currentConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2}?units={3}&extend={4}&exclude={5}&lang={6}";
-        private const string specificTimeConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2},{3?units={4}&extend={5}&exclude={6}&lang={7}";
+        /// <summary>
+        /// The API endpoint to retrieve weather conditions at a particular date and time.
+        /// {0} - API key.
+        /// {1} - Latitude.
+        /// {2} - Longitude.
+        /// {3} - Time to retrieve, either Unix time or [YYYY]-[MM]-[DD]T[HH]:[MM]:[SS].
+        /// {4} - Units.
+        /// {5} - Extends hourly data to include the next 7 days (if "hourly" is given).
+        /// {6} - Any blocks to be excluded from the results.
+        /// {7} - The language to be used in text summaries. 
+        /// </summary>
+        private const string SpecificTimeConditionsUrl = "https://api.forecast.io/forecast/{0}/{1},{2},{3?units={4}&extend={5}&exclude={6}&lang={7}";
+
+        private string apiKey;
 
         public ForecastApi(string key)
         {
