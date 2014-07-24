b43ab6695839c896a5b8aa98c4f87a69fe7ac698
Jerome Cheng <jerome@ayulin.net>
2014-07-24 11:07:37 +0800

    Add Forecast class.
    This models a response from the service.

---
+++ Forecast.io PCL/Models/Forecast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForecastIOPortable.Models
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class Forecast
+    {
+        [DataMember(Name = "latitude")]
+        public float Latitude { get; set; }
+
+        [DataMember(Name = "longitude")]
+        public float Longitude { get; set; }
+
+        [DataMember(Name = "timezone")]
+        public string TimeZone { get; set; }
+
+        [DataMember(Name = "currently")]
+        public CurrentDataPoint Currently { get; set; }
+
+        [DataMember(Name = "minutely")]
+        public MinuteForecast Minutely { get; set; }
+
+        [DataMember(Name = "hourly")]
+        public HourForecast Hourly { get; set; }
+
+        [DataMember(Name = "flags")]
+        public Flags Flags { get; set; }
+    }
+}
