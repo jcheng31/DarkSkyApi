db210a9daa33a43d3d4f67bb6d6466225069fd59
Jerome Cheng <jerome@ayulin.net>
2014-07-24 15:46:53 +0800

    Add header comment for ForecastApi.

--- Forecast.io PCL/ForecastApi.cs
+++ Forecast.io PCL/ForecastApi.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ForecastIOPortable
+﻿namespace ForecastIOPortable
 {
+    /// <summary>
+    /// Represents the Forecast.IO service.
+    /// </summary>
     public class ForecastApi
     {
         /// <summary>
