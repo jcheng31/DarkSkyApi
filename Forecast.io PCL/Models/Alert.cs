04587dae6b173059bb832285f91d10067cdc7de9
Jerome Cheng <jerome@ayulin.net>
2014-07-24 14:46:47 +0800

    StyleCop compliance for Alert.

--- Forecast.io PCL/Models/Alert.cs
+++ Forecast.io PCL/Models/Alert.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ForecastIOPortable.Models
+﻿namespace ForecastIOPortable.Models
 {
+    using System;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// An severe weather alert issued by a weather service for a particular location.
+    /// </summary>
     [DataContract]
     public class Alert
     {
-        /// <summary>
-        /// Gets or sets a short text summary of this alert.
-        /// </summary>
-        [DataMember(Name = "title")]
-        public string Title { get; set; }
-
         /// <summary>
         /// Unix time at which this alert will expire.
         /// </summary>
         [DataMember]
         private int expires;
+
+        /// <summary>
+        /// Gets or sets a short text summary of this alert.
+        /// </summary>
+        [DataMember(Name = "title")]
+        public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the date and time at which this alert is no longer valid.
