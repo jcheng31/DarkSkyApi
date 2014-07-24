1806748362a2b599f0eb58eab93103262879a9ef
Jerome Cheng <jerome@ayulin.net>
2014-07-24 15:07:53 +0800

    Header comments for DailyForecast.

--- Forecast.io PCL/Models/DailyForecast.cs
+++ Forecast.io PCL/Models/DailyForecast.cs
@@ -4,17 +4,26 @@
     using System.Runtime.Serialization;
 
     /// <summary>
-    /// 
+    /// A day-by-day forecast.
     /// </summary>
     [DataContract]
     public class DailyForecast
     {
+        /// <summary>
+        /// Gets or sets a human-readable summary of the forecast.
+        /// </summary>
         [DataMember(Name = "summary")]
         public string Summary { get; set; }
 
+        /// <summary>
+        /// Gets or sets machine-readable text that can be used to select an icon to display.
+        /// </summary>
         [DataMember(Name = "icon")]
         public string Icon { get; set; }
 
+        /// <summary>
+        /// Gets or sets the individual days that make up this forecast.
+        /// </summary>
         [DataMember(Name = "data")]
         public IList<DayDataPoint> Days { get; set; }
     }
