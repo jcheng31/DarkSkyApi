f51ad00e6e3f8adae8b2d96f3437d8ec39e6ee90
Jerome Cheng <jerome@ayulin.net>
2014-07-25 10:11:45 +0800

    Header comments for MinuteDataPoint.

--- Forecast.io PCL/Models/MinuteDataPoint.cs
+++ Forecast.io PCL/Models/MinuteDataPoint.cs
@@ -3,12 +3,21 @@
     using System;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// The weather conditions for a particular minute.
+    /// </summary>
     [DataContract]
     public class MinuteDataPoint
     {
+        /// <summary>
+        /// Unix time at which this data point applies.
+        /// </summary>
         [DataMember]
         private int time;
 
+        /// <summary>
+        /// Gets or sets the time of this data point.
+        /// </summary>
         public DateTime Time
         {
             get
@@ -22,9 +31,15 @@
             }
         }
 
+        /// <summary>
+        /// Gets or sets the average expected precipitation assuming any precipitation occurs.
+        /// </summary>
         [DataMember(Name = "precipIntensity")]
         public float PrecipitationIntensity { get; set; }
 
+        /// <summary>
+        /// Gets or sets the probability of precipitation (from 0 to 1).
+        /// </summary>
         [DataMember(Name = "precipProbability")]
         public float PrecipitationProbability { get; set; }
     }
