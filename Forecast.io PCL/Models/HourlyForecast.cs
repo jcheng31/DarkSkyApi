b12fb389522cb0519d3d3f25955ece1039e5fde4
Jerome Cheng <jerome@ayulin.net>
2014-07-25 10:08:32 +0800

    Header comments for HourlyForecast.

--- Forecast.io PCL/Models/HourlyForecast.cs
+++ Forecast.io PCL/Models/HourlyForecast.cs
@@ -4,19 +4,27 @@
     using System.Runtime.Serialization;
 
     /// <summary>
-    /// The hourly forecast.
+    /// An hour-by-hour forecast.
     /// </summary>
     [DataContract]
     public class HourlyForecast
     {
+        /// <summary>
+        /// Gets or sets a human-readable summary of the forecast.
+        /// </summary>
         [DataMember(Name = "summary")]
         public string Summary { get; set; }
 
+        /// <summary>
+        /// Gets or sets machine-readable text that can be used to select an icon to display.
+        /// </summary>
         [DataMember(Name = "icon")]
         public string Icon { get; set; }
 
+        /// <summary>
+        /// Gets or sets the individual hours that make up this forecast.
+        /// </summary>
         [DataMember(Name = "data")]
         public IList<HourDataPoint> Hours { get; set; } 
-
     }
 }
