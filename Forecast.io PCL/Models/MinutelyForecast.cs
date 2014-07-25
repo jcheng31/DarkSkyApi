385f5467095e3808a7183dedba48dc91b6f7e338
Jerome Cheng <jerome@ayulin.net>
2014-07-25 10:13:35 +0800

    Header comments for MinutelyForecast.

--- Forecast.io PCL/Models/MinutelyForecast.cs
+++ Forecast.io PCL/Models/MinutelyForecast.cs
@@ -3,15 +3,27 @@
     using System.Collections.Generic;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// A minute-by-minute forecast.
+    /// </summary>
     [DataContract]
     public class MinutelyForecast
     {
+        /// <summary>
+        /// Gets or sets a human-readable summary of the forecast.
+        /// </summary>
         [DataMember(Name = "summary")]
         public string Summary { get; set; }
 
+        /// <summary>
+        /// Gets or sets machine-readable text that can be used to select an icon to display.
+        /// </summary>
         [DataMember(Name = "icon")]
         public string Icon { get; set; }
 
+        /// <summary>
+        /// Gets or sets the individual minutes that make up this forecast.
+        /// </summary>
         [DataMember(Name = "data")]
         public IList<MinuteDataPoint> Minutes { get; set; }
     }
