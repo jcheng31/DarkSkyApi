43cf95e0c2bffdb47882691ff6bef4cc4647e1a5
Jerome Cheng <jerome@ayulin.net>
2014-07-25 12:14:41 +0800

    Remove unused usings.

--- ForecastPCL.Test/ApiTests.cs
+++ ForecastPCL.Test/ApiTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ForecastPCL.Test
+﻿namespace ForecastPCL.Test
 {
     using System.Configuration;
 
