a6db67836c56c986f6e3d586788ccf2f422a10ba
Jerome Cheng <jerome@ayulin.net>
2015-08-09 13:13:05 +0800

    Add failing test to check that language enums have values.

---
+++ ForecastPCL.Test/LanguageTests.cs
@@ -0,0 +1,26 @@
+﻿namespace ForecastPCL.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using ForecastIOPortable;
+
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class LanguageTests
+    {
+        [Test]
+        public void AllLanguagesHaveValues()
+        {
+            foreach (Language language in Enum.GetValues(typeof(Language)))
+            {
+                Assert.That(() => language.ToValue(), Throws.Nothing);
+            }
+        }
+    }
+}
