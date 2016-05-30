6df677340e8930f48428712f3102ff5efb621631
Jerome Cheng <jerome@ayulin.net>
2016-05-30 22:49:42 +0800

    Update dependencies and supported platforms.
    * Created a new Visual Studio solution and project files, which should
      fix the issues we were having with .NET Native.
    * Dropped support for Silverlight, but added Xamarin Android/iOS and ASP.NET Core.
    * Updated to the latest versions of Microsoft.Bcl and Microsoft.Net.Http.

--- ForecastPCL.Test/Properties/AssemblyInfo.cs
+++ ForecastPCL.Test/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ForecastPCL.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bcfd3de4-fa2f-4513-8f36-5b764b8159bc")]
+[assembly: Guid("8c9ec2dd-8438-4c7f-a489-f988698a1809")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ForecastPCL/Properties/AssemblyInfo.cs
+++ ForecastPCL/Properties/AssemblyInfo.cs
@@ -3,13 +3,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ForecastPCL")]
-[assembly: AssemblyDescription("A portable class library for the Forecast.io API. Supports .NET 4.5, Silverlight 5, Windows 8/8.1, Windows Phone 8 and 8.1, and Universal Windows Apps.")]
+[assembly: AssemblyDescription("An unofficial PCL for the forecast.io weather API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jerome Cheng")]
 [assembly: AssemblyProduct("ForecastPCL")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -19,12 +19,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
