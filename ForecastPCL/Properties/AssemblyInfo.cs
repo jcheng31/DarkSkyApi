c14021deb509e05a77b78230e9aea3247d2e6052
Jerome Cheng <jerome@ayulin.net>
2016-09-27 21:12:18 +0800

    Update assembly info.

--- ForecastPCL/Properties/AssemblyInfo.cs
+++ ForecastPCL/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ForecastPCL")]
-[assembly: AssemblyDescription("An unofficial PCL for the forecast.io weather API.")]
+[assembly: AssemblyTitle("DarkSkyApi")]
+[assembly: AssemblyDescription("An unofficial PCL for the Dark Sky weather API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jerome Cheng")]
-[assembly: AssemblyProduct("ForecastPCL")]
+[assembly: AssemblyProduct("DarkSkyApi")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
